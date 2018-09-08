b11b72de8ae2dee9bcce28055f8a9def53ffb9d3
hadv <dvietha@gmail.com>
2018-09-08 11:10:24 +0700

    params: refactor the position of TxGasPrice

--- params/protocol_params.go
+++ params/protocol_params.go
@@ -29,7 +29,6 @@ const (
 	CallValueTransferGas  uint64 = 9000  // Paid for CALL when the value transfer is non-zero.
 	CallNewAccountGas     uint64 = 25000 // Paid for CALL when the destination address didn't exist prior.
 	TxGas                 uint64 = 21000 // Per transaction not creating a contract. NOTE: Not payable on data of calls between transactions.
-	TxGasPrice                   = 0     // Zero Transaction Fee
 	TxGasContractCreation uint64 = 53000 // Per transaction that creates a contract. NOTE: Not payable on data of calls between transactions.
 	TxDataZeroGas         uint64 = 4     // Per byte of data attached to a transaction that equals zero. NOTE: Not payable on data of calls between transactions.
 	QuadCoeffDiv          uint64 = 512   // Divisor for the quadratic particle of the memory cost equation.
@@ -60,6 +59,7 @@ const (
 	TxDataNonZeroGas uint64 = 68    // Per byte of data attached to a transaction that is not equal to zero. NOTE: Not payable on data of calls between transactions.
 
 	MaxCodeSize = 24576 // Maximum bytecode to permit for a contract
+	TxGasPrice  = 0     // Zero Transaction Fee
 
 	// Precompiled contract gas prices
 
