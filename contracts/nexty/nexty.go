f89ae2f345f706b3fa233b5ce5278f79cbfee1fd
hadv <dvietha@gmail.com>
2019-02-15 17:50:58 +0700

    contracts: add binding for nexty token foundation contract

--- contracts/nexty/nexty.go
+++ contracts/nexty/nexty.go
@@ -17,3 +17,4 @@
 package nexty
 
 //go:generate ../../build/bin/abigen --sol contract/nexty.sol --pkg contract --out contract/nexty.go
+//go:generate ../../build/bin/abigen --sol contract/token.sol --pkg contract --out contract/token.go
