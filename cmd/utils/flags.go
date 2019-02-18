176ef087a2b59357a8d9e5bc17cb92945d39d9a8
hadv <dvietha@gmail.com>
2019-02-18 11:08:54 +0700

    add config and genesis for official nexty testnet

--- cmd/utils/flags.go
+++ cmd/utils/flags.go
@@ -1278,7 +1278,7 @@ func SetEthConfig(ctx *cli.Context, stack *node.Node, cfg *eth.Config) {
 	switch {
 	case ctx.GlobalBool(TestnetFlag.Name):
 		if !ctx.GlobalIsSet(NetworkIdFlag.Name) {
-			cfg.NetworkId = 3
+			cfg.NetworkId = 111111
 		}
 		cfg.Genesis = core.DefaultTestnetGenesisBlock()
 	case ctx.GlobalBool(RinkebyFlag.Name):
