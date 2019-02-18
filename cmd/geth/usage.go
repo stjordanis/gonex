1b93bcfebec419f20e52dd09dddf7aacea3c9590
Zergity <zergity@gmail.com>
2019-02-18 10:51:19 +0700

    cmd/geth: add missing cli usage for  noremote

--- cmd/geth/usage.go
+++ cmd/geth/usage.go
@@ -118,6 +118,7 @@ var AppHelpFlagGroups = []flagGroup{
 		Flags: []cli.Flag{
 			utils.TxPoolLocalsFlag,
 			utils.TxPoolNoLocalsFlag,
+			utils.TxPoolNoRemotesFlag,
 			utils.TxPoolJournalFlag,
 			utils.TxPoolRejournalFlag,
 			utils.TxPoolPriceLimitFlag,
