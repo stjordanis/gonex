fc0d111e216254c744998ba5c18f7062f6fa7a32
Zergity <zergity@gmail.com>
2019-02-22 17:09:11 +0700

    contracts/nexty/contract: governance project

--- contracts/nexty/contract/nexty.sol
+++ contracts/nexty/contract/nexty.sol
@@ -1,90 +1,7 @@
 pragma solidity ^0.5.0;
 
-/**
- * @title SafeMath
- * @dev Unsigned math operations with safety checks that revert on error
- */
-library SafeMath {
-    /**
-     * @dev Multiplies two unsigned integers, reverts on overflow.
-     */
-    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-        // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
-        // benefit is lost if 'b' is also tested.
-        // See: https://github.com/OpenZeppelin/openzeppelin-solidity/pull/522
-        if (a == 0) {
-            return 0;
-        }
-
-        uint256 c = a * b;
-        require(c / a == b);
-
-        return c;
-    }
-
-    /**
-     * @dev Integer division of two unsigned integers truncating the quotient, reverts on division by zero.
-     */
-    function div(uint256 a, uint256 b) internal pure returns (uint256) {
-        // Solidity only automatically asserts when dividing by 0
-        require(b > 0);
-        uint256 c = a / b;
-        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-
-        return c;
-    }
-
-    /**
-     * @dev Subtracts two unsigned integers, reverts on overflow (i.e. if subtrahend is greater than minuend).
-     */
-    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-        require(b <= a);
-        uint256 c = a - b;
-
-        return c;
-    }
-
-    /**
-     * @dev Adds two unsigned integers, reverts on overflow.
-     */
-    function add(uint256 a, uint256 b) internal pure returns (uint256) {
-        uint256 c = a + b;
-        require(c >= a);
-
-        return c;
-    }
-
-    /**
-     * @dev Divides two unsigned integers and returns the remainder (unsigned integer modulo),
-     * reverts when dividing by zero.
-     */
-    function mod(uint256 a, uint256 b) internal pure returns (uint256) {
-        require(b != 0);
-        return a % b;
-    }
-}
-
-/**
- * @title ERC20 interface
- * @dev see https://github.com/ethereum/EIPs/issues/20
- */
-interface IERC20 {
-    function transfer(address to, uint256 value) external returns (bool);
-
-    function approve(address spender, uint256 value) external returns (bool);
-
-    function transferFrom(address from, address to, uint256 value) external returns (bool);
-
-    function totalSupply() external view returns (uint256);
-
-    function balanceOf(address who) external view returns (uint256);
-
-    function allowance(address owner, address spender) external view returns (uint256);
-
-    event Transfer(address indexed from, address indexed to, uint256 value);
-
-    event Approval(address indexed owner, address indexed spender, uint256 value);
-}
+import "node_modules/openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "node_modules/openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
 
 /**
  * @title Nexty sealers management smart contract
@@ -92,12 +9,10 @@ interface IERC20 {
 contract NextyGovernance {
     using SafeMath for uint256;
 
-    // minimum of deposited NTF to join
-    uint256 public constant MIN_NTF_AMOUNT = 100;
-    // minimum blocks number distance from last leaved to current chain blocknumber to withdrawable
-    uint256 public constant LOCK_DURATION = 5 * 60;
+    // zero address
+    address constant ZERO_ADDRESS = address(0x0);
 
-    enum SealerStatus {
+    enum Status {
         PENDING_ACTIVE,     // Sealer deposited enough NTFs into registration contract successfully.
 
         ACTIVE,             // Sealer send request to become a sealer 
@@ -113,31 +28,42 @@ contract NextyGovernance {
                             //and cannot become active sealer and cannot withdraw balance neither.
     }
 
-    struct Record {
-        SealerStatus status;
-        //ntf amount deposited
+    struct Account {
+        Status status;
+        // ntf amount deposited
         uint256 balance;
-        //sealer used address to seal blocks
-        address coinbase;
-        //withdrawable time after leaving
-        uint256 unlockTime;
+        // delegated address to seal blocks
+        address signer;
+        // withdrawable block number after leaving
+        uint256 unlockHeight;
     }
+
+    // Consensus variables
+
+    // index = 0
+    // signers array
+    address[] public signers;
+
+    // index = 1
+    // signer => coinbase (beneficiary address) map
+    mapping(address => address) public signerCoinbase;
+
+    // End of consensus variables
+
+    // coinbase => NTF Account map
+    mapping(address => Account) public account;
+
+    // minimum of deposited NTF to join
+    uint256 public stakeRequire;
+    // minimum number of blocks signer has to wait from leaving block to withdraw the fund
+    uint256 public stakeLockHeight;
 
     // NTF token contract, unit used to join Nexty sealers
     IERC20 public token;
 
-    // get invester address of a coinbase
-    mapping(address => Record) public sealers;
-
-    address[] public coinbases;
-    // check if address of a coinbase
-    mapping(address => bool) public isCoinbase;
-    // get sealer's address of a coinbase address
-    mapping(address => address) public getSealer;
-
     event Deposited(address _sealer, uint _amount);
-    event Joined(address _sealer, address _coinbase);
-    event Left(address _sealer, address _coinbase);
+    event Joined(address _sealer, address _signer);
+    event Left(address _sealer, address _signer);
     event Withdrawn(address _sealer, uint256 _amount);
     event Banned(address _sealer);
     event Unbanned(address _sealer);
@@ -148,27 +74,27 @@ contract NextyGovernance {
     * - must not be the token address
     * - must not be the sender's address
     */
-    modifier validCoinbase(address _coinbase) {
-        require(!isCoinbase[_coinbase], "coinbase already used");
-        require(_coinbase != address(0x0), "coinbase zero");
-        require(_coinbase != address(this), "same contract's address");
-        require(_coinbase != msg.sender, "same sender's address");
+    modifier validSigner(address _signer) {
+        require(signerCoinbase[_signer] != ZERO_ADDRESS, "signer already used");
+        require(_signer != ZERO_ADDRESS, "signer zero");
+        require(_signer != address(this), "same contract's address");
+        require(_signer != msg.sender, "same sender's address");
         _;
     }
 
     modifier notBanned() {
-        require(sealers[msg.sender].status != SealerStatus.PENALIZED, "banned ");
+        require(account[msg.sender].status != Status.PENALIZED, "banned ");
         _;
     }
 
     modifier joinable() {
-        require(sealers[msg.sender].status != SealerStatus.ACTIVE, "already joined ");
-        require(sealers[msg.sender].balance >= MIN_NTF_AMOUNT, "not enough ntf");
+        require(account[msg.sender].status != Status.ACTIVE, "already joined ");
+        require(account[msg.sender].balance >= stakeRequire, "not enough ntf");
         _;
     }
 
     modifier leaveable() {
-        require(sealers[msg.sender].status == SealerStatus.ACTIVE, "not joined ");
+        require(account[msg.sender].status == Status.ACTIVE, "not joined ");
         _;
     }
 
@@ -180,35 +106,34 @@ contract NextyGovernance {
     /**
     * contract initialize
     */
-    constructor(address _token, address[] memory _sealers) public {
+    constructor(address _token, uint256 _stakeRequire, uint256 _stakeLockHeight, address[] memory _signers) public {
         token = IERC20(_token);
-        for (uint i = 0; i < _sealers.length; i++) {
-            coinbases.push(_sealers[i]);
-            isCoinbase[_sealers[i]] = true;
-            getSealer[_sealers[i]] = _sealers[i];
-            sealers[_sealers[i]].coinbase = _sealers[i];
-            sealers[_sealers[i]].status = SealerStatus.ACTIVE;    
-        }        
+        stakeRequire = _stakeRequire;
+        stakeLockHeight = _stakeLockHeight;
+        for (uint i = 0; i < _signers.length; i++) {
+            signers.push(_signers[i]);
+            signerCoinbase[_signers[i]] = _signers[i];
+            account[_signers[i]].signer = _signers[i];
+            account[_signers[i]].status = Status.ACTIVE;    
+        }
     }
 
     // Get ban status of a sealer's address
     function isBanned(address _address) public view returns(bool) {
-        return (sealers[_address].status == SealerStatus.PENALIZED);
+        return (account[_address].status == Status.PENALIZED);
     }
 
     ////////////////////////////////
 
-    function addCoinbase(address _coinbase) internal {
-        isCoinbase[_coinbase] = true;
-        coinbases.push(_coinbase);
+    function addSigner(address _signer) internal {
+        signers.push(_signer);
     }
 
-    function removeCoinbase(address _coinbase) internal {
-        isCoinbase[_coinbase] = false;
-        for (uint i = 0; i < coinbases.length; i++) {
-            if (_coinbase == coinbases[i]) {
-                coinbases[i] = coinbases[coinbases.length - 1];
-                coinbases.length--;
+    function removeSigner(address _signer) internal {
+        for (uint i = 0; i < signers.length; i++) {
+            if (_signer == signers[i]) {
+                signers[i] = signers[signers.length - 1];
+                signers.length--;
                 return;
             }
         }
@@ -221,7 +146,7 @@ contract NextyGovernance {
     */
     function deposit(uint256 _amount) public returns (bool) {
         token.transferFrom(msg.sender, address(this), _amount);
-        sealers[msg.sender].balance = (sealers[msg.sender].balance).add(_amount);
+        account[msg.sender].balance = (account[msg.sender].balance).add(_amount);
         emit Deposited(msg.sender, _amount);
         return true;
     }
@@ -229,15 +154,15 @@ contract NextyGovernance {
     /**
     * To allow deposited NTF participate joining in as sealer. 
     * Participate already must deposit enough NTF via Deposit function. 
-    * It takes coinbase as parameter.
-    * @param _coinbase Destination address
+    * It takes signer as parameter.
+    * @param _signer Destination address
     */
-    function join(address _coinbase) public notBanned joinable validCoinbase(_coinbase) returns (bool) {
-        sealers[msg.sender].coinbase = _coinbase;
-        sealers[msg.sender].status = SealerStatus.ACTIVE;
-        getSealer[_coinbase] = msg.sender;
-        addCoinbase(_coinbase);
-        emit Joined(msg.sender, _coinbase);
+    function join(address _signer) public notBanned joinable validSigner(_signer) returns (bool) {
+        account[msg.sender].signer = _signer;
+        account[msg.sender].status = Status.ACTIVE;
+        signerCoinbase[_signer] = msg.sender;
+        addSigner(_signer);
+        emit Joined(msg.sender, _signer);
         return true;
     }
 
@@ -245,14 +170,14 @@ contract NextyGovernance {
     * Request to exit out of activation sealer set
     */
     function leave() public notBanned leaveable returns (bool) {
-        address _coinbase = sealers[msg.sender].coinbase;
+        address _signer = account[msg.sender].signer;
 
-        sealers[msg.sender].coinbase = 0x0000000000000000000000000000000000000000;
-        sealers[msg.sender].status = SealerStatus.PENDING_WITHDRAW;
-        sealers[msg.sender].unlockTime = LOCK_DURATION.add(block.timestamp);
-        delete getSealer[_coinbase];
-        removeCoinbase(_coinbase);
-        emit Left(msg.sender, _coinbase);
+        account[msg.sender].signer = ZERO_ADDRESS;
+        account[msg.sender].status = Status.PENDING_WITHDRAW;
+        account[msg.sender].unlockHeight = stakeLockHeight.add(block.number);
+        delete signerCoinbase[_signer];
+        removeSigner(_signer);
+        emit Left(msg.sender, _signer);
         return true;
     }
 
@@ -260,42 +185,42 @@ contract NextyGovernance {
     * To withdraw sealer’s NTF balance when they already exited and after withdrawal period.
     */
     function withdraw() public notBanned withdrawable returns (bool) {
-        uint256 amount = sealers[msg.sender].balance;
-        sealers[msg.sender].balance = 0;
-        sealers[msg.sender].status = SealerStatus.WITHDRAWN;        
+        uint256 amount = account[msg.sender].balance;
+        account[msg.sender].balance = 0;
+        account[msg.sender].status = Status.WITHDRAWN;        
         token.transfer(msg.sender, amount);
         emit Withdrawn(msg.sender, amount);
         return true;
     }
 
-    function getStatusCode(SealerStatus _status) private pure returns(uint256){
-        if (_status == SealerStatus.PENDING_ACTIVE) return 0;
-        if (_status == SealerStatus.ACTIVE) return 1;
-        if (_status == SealerStatus.PENDING_WITHDRAW) return 2;
-        if (_status == SealerStatus.WITHDRAWN) return 3;
+    function getStatusCode(Status _status) private pure returns(uint256){
+        if (_status == Status.PENDING_ACTIVE) return 0;
+        if (_status == Status.ACTIVE) return 1;
+        if (_status == Status.PENDING_WITHDRAW) return 2;
+        if (_status == Status.WITHDRAWN) return 3;
         return 127;
     }
 
     function getStatus(address _address) public view returns(uint256) {
-        return getStatusCode(sealers[_address].status);
+        return getStatusCode(account[_address].status);
     }
 
     function getBalance(address _address) public view returns(uint256) {
-        return sealers[_address].balance;
+        return account[_address].balance;
     }  
 
     function getCoinbase(address _address) public view returns(address) {
-        return sealers[_address].coinbase;
+        return account[_address].signer;
     }  
 
-    function getUnlockTime(address _address) public view returns(uint256) {
-        return sealers[_address].unlockTime;
+    function getUnlockHeight(address _address) public view returns(uint256) {
+        return account[_address].unlockHeight;
     }
 
     function isWithdrawable(address _address) public view returns(bool) {
-        return  
-        (sealers[_address].status != SealerStatus.ACTIVE) &&
-        (sealers[_address].status != SealerStatus.PENALIZED) &&
-        (sealers[_address].unlockTime < block.timestamp);
+        return 
+        (account[_address].status != Status.ACTIVE) && 
+        (account[_address].status != Status.PENALIZED) && 
+        (account[_address].unlockHeight < block.number);
     }
 }
